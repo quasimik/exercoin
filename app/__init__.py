3633dd99c06fa9a2e877723e4031168757aeda0a
Matthew Ung <matthew.ung814@gmail.com>
2018-02-03 13:20:26 -0800

    changed structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from web3 import Web3, HTTPProvider, TestRPCProvider
+from web3 import Web3, HTTPProvider, RPCProvider
+import json
+import os.path
 
-web3 = Web3(TestRPCProvider(host='localhost', port='8545'))
+web3 = Web3(RPCProvider(host='localhost', port='8545'))
 
-with open('coin.abi', 'r') as abi_definition:
+with open('./sol/coin.abi', 'r') as abi_definition:
 	abi = json.load(abi_definition)
 contract_address = 0x2dee7a447dc2376c9159ff4b4caf3ae0c6cc31cd
 contract = web3.eth.contract(abi, contract_address)
@@ -15,4 +17,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
+
+
