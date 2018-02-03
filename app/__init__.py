2ae3f82f06d8034c0e175235b3e10d2461756041
Matthew Ung <matthew.ung814@gmail.com>
2018-02-03 13:56:16 -0800

    added views

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,21 @@
 from flask import Flask
 from web3 import Web3, HTTPProvider, RPCProvider
 import json
-import os.path
 
-web3 = Web3(RPCProvider(host='localhost', port='8545'))
+web3 = Web3(HTTPProvider("http://localhost:5000"))
 
 with open('./sol/coin.abi', 'r') as abi_definition:
-	abi = json.load(abi_definition)
-contract_address = 0x2dee7a447dc2376c9159ff4b4caf3ae0c6cc31cd
+    abi = json.load(abi_definition)
+contract_address = '0x2dee7a447dc2376c9159ff4b4caf3ae0c6cc31cd'
 contract = web3.eth.contract(abi, contract_address)
 
-
-# block = web3.eth.getBlock('latest')
+"""print(web3.eth.defaultAccount)
+print(web3.eth.accounts)
+print(web3.eth.accounts[0])
+"""
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello World!"
 
+from app import views
 
