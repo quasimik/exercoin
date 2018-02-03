691dcc1eda905452c2f2abaa460228340fa6a708
willyjlee <williamjlee@ucla.edu>
2018-02-03 12:15:55 -0800

    not working yet

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from web3 import Web3, HTTPProvider
+
+web3 = Web3(HTTPProvider('http://localhost:5000'))
+block = web3.eth.getBlock('latest')
+
 app = Flask(__name__)
 
 @app.route("/")
