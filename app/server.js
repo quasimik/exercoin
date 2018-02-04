3812fade130235de0e35032bfad987db79b6a365
Matthew Ung <matthew.ung814@gmail.com>
2018-02-03 16:02:48 -0800

    work

--- app/server.js
+++ app/server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-import { abi, address } from './EthConfig';
+const path = require("path")
+// import { abi, address } from './EthConfig';
 
 const app = express()
-
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.render('index')
+    res.render('index');
 })
 
 app.post('/', function(req, res){
@@ -21,3 +21,5 @@ app.post('/', function(req, res){
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
 })
+
+
