6edb32c7fe6482dfba9544382cb0cf7223eb9e4d
Michael Liu <mike666234@gmail.com>
2018-02-03 22:46:16 -0800

    Add counters and video stream to client

--- halecoin-client/src/App.js
+++ halecoin-client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import fire from './fire';
 
 // class App extends Component {
@@ -63,11 +64,80 @@ class App extends Component {
 
   render() {
     return (
-        <form onSubmit={this.incPuCount.bind(this)}>
-          <input type="submit"/>
-          <p>{this.state.pushUpCount}</p>
-        </form>
+      <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Grid>
+
+          <Row>
+            <Col xsOffset={3} xs={3}>
+              <h1 style={{ fontFamily: 'Berkshire Swash' }}>Push-ups</h1>
+            </Col>
+            <Col xs={3}>
+              <h1 style={{ fontFamily: 'Berkshire Swash' }}>HaleCoins</h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xsOffset={3} xs={3}>
+              <h1 style={{ fontFamily: 'Lato', fontSize: '4em' }}>{this.state.pushUpCount}</h1>
+            </Col>
+            <Col xs={3}>
+              <h1 style={{ fontFamily: 'Lato', fontSize: '4em' }}>{this.state.pushUpCount}</h1>
+            </Col>
+          </Row>
+
+          <Row><Col><div style={{ padding: '2em' }} /></Col></Row>
+
+          <Row>
+            <Col xsOffset={3} xs={6}>
+              <img style={{ width: '500px', height: '300px' }} src="http://169.234.110.162:8080/video" />
+            </Col>
+          </Row>
+
+        </Grid>
+      </div>
     );
+    // return (
+    //     <Grid>
+    //       <Row className="show-grid">
+    //         <Col xs={12} md={8}>
+    //           <form onSubmit={this.incPuCount.bind(this)}>
+    //             <input type="submit"/>
+    //             <p>{this.state.pushUpCount}</p>
+    //           </form>
+    //         </Col>
+    //         <Col xs={6} md={4}>
+    //           <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+    //         </Col>
+    //       </Row>
+
+    //       <Row className="show-grid">
+    //         <Col xs={6} md={4}>
+    //           <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+    //         </Col>
+    //         <Col xs={6} md={4}>
+    //           <code>&lt;{'Col xs={6} md={4}'} /&gt;</code>
+    //         </Col>
+    //         <Col xsHidden md={4}>
+    //           <code>&lt;{'Col xsHidden md={4}'} /&gt;</code>
+    //         </Col>
+    //       </Row>
+
+    //       <Row className="show-grid">
+    //         <Col xs={6} xsOffset={6}>
+    //           <code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code>
+    //         </Col>
+    //       </Row>
+
+    //       <Row className="show-grid">
+    //         <Col md={6} mdPush={6}>
+    //           <code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code>
+    //         </Col>
+    //         <Col md={6} mdPull={6}>
+    //           <code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code>
+    //         </Col>
+    //       </Row>
+    //     </Grid>
+    // );
   }
 }
 
