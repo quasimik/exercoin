0907e765be4ec6033f8008a2c614218e841c87ea
willyjlee <williamjlee@ucla.edu>
2018-02-03 16:36:55 -0800

    in progress earn coin

--- app/server.js
+++ app/server.js
@@ -13,10 +13,6 @@ app.get('/', function(req, res) {
     res.render('index');
 })
 
-app.post('/', function(req, res){
-    res.render('index');
-    console.log(req.body.city);
-})
 
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')
